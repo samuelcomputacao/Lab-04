2ad1745434d3a9b6dc7f02fb49b81a32c814552b
samuelcomputacao <samuel25101998@gmail.com>
2018-06-01 23:17:15 -0300

    Documentando a classe "MAIN"

--- src/com/samuel/lab4/principal/Main.java
+++ src/com/samuel/lab4/principal/Main.java
@@ -6,8 +6,16 @@ import java.io.FileNotFoundException;
 import com.samuel.lab4.model.ControleAcademico;
 import com.samuel.lab4.view.Menu;
 
+/**
+ * Classe responsável por iniciar o controle acadêmico
+ * @author Samuel Pereira de Vasconcelos
+ *
+ */
 public class Main {
-	
+
+	/**
+	 * Método pricipal e que é chamado para iniciar o controle acadêmico
+	 */
 	public static void main(String[] args) {
 		ControleAcademico academico = new ControleAcademico();
 		try {
