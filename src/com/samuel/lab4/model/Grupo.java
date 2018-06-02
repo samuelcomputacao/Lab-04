f14776f9dc3e58c6727676c0b9a0881f372c3af9
samuelcomputacao <samuel25101998@gmail.com>
2018-06-01 22:08:19 -0300

    Documentando a classe "GRUPO"

--- src/com/samuel/lab4/model/Grupo.java
+++ src/com/samuel/lab4/model/Grupo.java
@@ -7,17 +7,39 @@ import java.util.Set;
 
 import com.samuel.lab4.exception.CampoVazioException;
 
+/**
+ * Classe que representa um grupo no sistema
+ * 
+ * @author Samuel Pereira de Vasconcelos
+ *
+ */
 public class Grupo {
 	
+	/**
+	 * Representa o nome do grupo
+	 */
 	private String nome;
+	
+	/**
+	 * Represeta todos os alunos que estão alocados neste grupo 
+	 */
 	private Set<Aluno> alunos;
 
+	/**
+	 * Método responsável por inicializar um grupo no sistema
+	 * @param nome : Uma String representando o nome do grupo
+	 * @throws CampoVazioException : Uma exceção que é lançada quando o nome do grupo recebido é nulo
+	 */
 	public Grupo(String nome) throws CampoVazioException {
 		if(nome==null)throw new CampoVazioException("CAMPO NOME DO GRUPO VAZIO");
 		this.nome = nome;
 		this.alunos= new HashSet<Aluno>();
 	}
 
+	/**
+	 * Método reponsável por criar um inteiro que represente o grupo de maneira única no sistema a partir do seu nome
+	 * @return Um inteiro representando o grupo
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -26,6 +48,10 @@ public class Grupo {
 		return result;
 	}
 
+	/**
+	 * Método responsável por comparar dois Grupos a partir de seus nomes
+	 * @return Retorna um valor bolleano que indica se eles tem o mesmo nome ou não
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -42,16 +68,30 @@ public class Grupo {
 			return false;
 		return true;
 	}
-
+	
+	/**
+	 * Método acessível para pegar o nome do grupo 
+	 * @return Uma String representando o nome do grupo
+	 */
 	public String getNome() {
 		return this.nome;
 	}
-
+	
+	/**
+	 * Método responsável por alocar um aluno ao grupo
+	 * @param aluno : Um aluno que será alocado no grupo
+	 * @return Um valor bolleano que indica se foi possível alocar o aluno ou não
+	 * @throws CampoVazioException : Retorna esta exceção se o aluno passado como parâmetro for nulo
+	 */
 	public boolean alocar(Aluno aluno) throws CampoVazioException {
 		if(aluno==null) throw new CampoVazioException("CAMPO ALUNO VAZIO");
 		return alunos.add(aluno);
 	}
-
+	
+	/**
+	 * Método reponsável por criar uma lista de String que representa todo os alunos alocados no grupo
+	 * @return Uma lista de String que representa todos os alunos que estão alocados no grupo
+	 */
 	public List<String> listaAlocados() {
 		List<String> result = new ArrayList<>();
 		for(Aluno aluno:alunos) {
@@ -60,6 +100,4 @@ public class Grupo {
 		return result;
 	}
 	
-	
-
 }
